f2b2f3623f49d01546c15eda1b96407649055a9d
johnnywishwash <johnnywishwash@gmail.com>
2018-02-10 09:45:59 +0000

    Added testing

---
+++ deck_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 40 {
+		t.Errorf("Expected deck length of 40, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Ten of Diamonds" {
+		t.Errorf("Expected first card of Ten of Diamonds, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 40 {
+		t.Errorf("Expected deck length of 40, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
